8f5693020559241b8d28488fc6abf15597d88072
Zhenyun Huang <3291841183@qq.com>
2019-02-26 15:42:16 +0800

    Update Searchinsertposition.java

--- Searchinsertposition.java
+++ Searchinsertposition.java
@@ -37,3 +37,48 @@ class Solution1 {
         return 0;
     }
 }
+
+class Solution2 {
+    public int searchInsert(int[] nums, int target) {
+        int L=nums.length;
+        if(L==0){
+            return 0;
+        }
+        if(L==1){
+            if(target<nums[0]){
+                return 0;
+            }
+            if(target>nums[0]){
+                return 1;
+            }  
+        }
+        if(target<nums[0]){
+            return 0;
+        }
+        if(target>nums[L-1]){
+            return L;
+        }
+        
+        int low=0;
+        int high=L-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                if(nums[mid+1]>target){
+                    return mid+1;
+                }
+                low=mid+1;
+            }
+            else{
+                if(nums[mid-1]<target){
+                    return mid;
+                }
+                high=mid-1;
+            }
+        }
+        return 0;
+    }
+}
