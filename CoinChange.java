956c8912462cc10993d032e213929ad2ef0bee9b
Zhenyun Huang <3291841183@qq.com>
2019-03-08 16:54:25 +0800

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -6,11 +6,11 @@
 			if (L == 0) {
 				return -1;
 			}
-			int dp[] = new int[amount + 1];
-			for (int i = 0; i <= amount; i++) {
+			int dp[] = new int[amount + 1];//build an array to store the answer for each amount i
+			for (int i = 0; i <= amount; i++) {//set all dp for each amount i as -1
 				dp[i] = -1;
 			}
-			dp[0] = 0;
+			dp[0] = 0;//when amount = 0, the answer must be 0
 			for (int i = 1; i <= amount; i++) {
 				for (int j = 0; j < L; j++) {
 					if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
