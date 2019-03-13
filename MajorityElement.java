21ade7a543c8f30bbfdb58aa401e24c9141680c8
Zhenyun Huang <3291841183@qq.com>
2019-03-13 23:30:17 +0800

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -30,3 +30,23 @@ class SolutionofLeetCode169 {
 
 	}
 }
+
+// Best solution by leetcoders
+
+class Solution {
+	public int majorityElement(int[] nums) {
+		int count = 1;
+		int maj = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (maj == nums[i])
+				count++;
+			else {
+				count--;
+				if (count == 0) {
+					maj = nums[i + 1];
+				}
+			}
+		}
+		return maj;
+	}
+}
