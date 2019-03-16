49a5d05e57c20091cb86a779a24107191f79c90e
Zhenyun Huang <3291841183@qq.com>
2019-03-16 15:05:33 +0800

    Create PowerofTwo.java

---
+++ PowerofTwo.java
@@ -0,0 +1,22 @@
+class SolutionofLeetCode231 {
+	public boolean isPowerOfTwo(int n) {
+		if (n == 0) {
+			return false;
+		}
+		if (n == 1) {
+			return true;
+		}
+		if (n % 2 != 0) {
+			return false;
+		}
+		while (n % 2 == 0) {
+			n = n / 2;
+		}
+		if (n != 1) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+}
