f262f062608fb92f65ac8ef7c1516b9ffc196827
Zhenyun Huang <3291841183@qq.com>
2019-05-31 00:01:34 +0800

    Update LowestCommonAncestorOfABinaryTree.java

--- LowestCommonAncestorOfABinaryTree.java
+++ LowestCommonAncestorOfABinaryTree.java
@@ -1,1 +1,27 @@
 //just the solution part
+
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null && right == null) {
+            return left;
+        }
+
+        if (right != null && left == null) {
+            return right;
+        }
+
+        return null;
+    }
+}
